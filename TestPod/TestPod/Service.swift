91c731120439b4523c62cfc35ce780f450a9effe
Chema <ggc@Googlings-MacBook-Pro.local>
2019-09-23 09:33:20 +0200

    dev pod

---
+++ TestPod/TestPod/Service.swift
@@ -0,0 +1,26 @@
+//
+//  Service.swift
+//  TestPod
+//
+//  Created by Chema on 23/09/2019.
+//  Copyright © 2019 Vueling. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func ShowText() -> String {
+        return "test Pod message!"
+        
+    }
+    
+    
+    
+    
+}
+
+
+
